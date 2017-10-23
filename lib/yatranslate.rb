dbc24f8f6991c07723cec35c32fffed72a95fb15
GeorSh <shepelevgeorg@gmail.com>
2017-10-23 17:04:23 +0300

    fixed

--- lib/yatranslate.rb
+++ lib/yatranslate.rb
@@ -2,7 +2,7 @@ require 'yatranslate/gateway'
 require 'yatranslate/version'
 
 #module Yatranslate
-  class Yatranslate
+  class Client
     
     #API_KEY = "trnsl.1.1.20170529T154416Z.047bb57fd7a8b3a1.97c5cf0ccade87d10fb983346cfb5703bf811b3e"
     #VERSION 1.5
